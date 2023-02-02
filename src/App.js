01f3fb8a5970c242505e70ed2c01fdcdb87bff74
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-02-02 16:18:17 +0200

    sec

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
-import Contact from './page/Contact';import Download from './page/Download';import Features from './page/Features';import Footer from './page/Footer';import HomePage from './page/HomePage';import Overview from './page/Overview';import Screen from './page/Screen';
+import { lazy } from 'react';
+const HomePage = lazy(() => import("./page/HomePage"));
+const Contact = lazy(() => import("./page/Contact"));
+const Download = lazy(() => import("./page/Download"));
+const Features = lazy(() => import("./page/Features"));
+const Footer = lazy(() => import("./page/Footer"));
+const Overview = lazy(() => import("./page/Overview"));
+const Screen = lazy(() => import("./page/Screen"));
+
 function App() {
- return ( <div className='App'><HomePage /><Features /><Overview /><Screen/><Download/><Contact/><Footer/></div>)}
+  return (
+    <div className='App'>
+      <HomePage />
+      <Features />
+      <Overview />  
+      <Screen/>
+      <Download/>
+      <Contact/>
+      <Footer/>
+    </div>
+
+  );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.scss'
+import './styles/App.scss';
 import ReactDOM from 'react-dom/client';
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
